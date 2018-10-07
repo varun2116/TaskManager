9162fa1157f1184bb4cdbce693e7db8999f84b6e
varun2116 <varun2116@gmail.com>
2018-10-07 23:08:04 +0530

    Updates

---
+++ precache-manifest.c37b8616b56bce04bfd29e662246672e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d56022c991c7255786b5",
+    "url": "/TaskManager/static/js/runtime~main.d56022c9.js"
+  },
+  {
+    "revision": "a486366ecd9da98460f1",
+    "url": "/TaskManager/static/js/main.a486366e.chunk.js"
+  },
+  {
+    "revision": "d8537f1acccc7f37cc80",
+    "url": "/TaskManager/static/js/1.d8537f1a.chunk.js"
+  },
+  {
+    "revision": "a486366ecd9da98460f1",
+    "url": "/TaskManager/static/css/main.3c4cbdc5.chunk.css"
+  },
+  {
+    "revision": "f9a2293ea43cfc73b020de221dea5374",
+    "url": "/TaskManager/index.html"
+  }
+];
